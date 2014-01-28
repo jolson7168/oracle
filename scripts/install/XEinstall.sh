256f95d3ad717fa7034e2448b2cdc872140ff521
jjo <jo6971@nododos.com>
2014-01-28 10:02:51 -0600

    added changes to put oracle info in oracle's .bashrc

--- scripts/install/XEinstall.sh
+++ scripts/install/XEinstall.sh
@@ -4,3 +4,11 @@
 unzip /u01/download/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
 rpm -ivh /u01/download/Disk1/oracle-xe-11.2.0-1.0.x86_64.rpm > /u01/logs/XEsilentinstall.log
 /etc/init.d/oracle-xe configure < /u01/git/oracle/scripts/install/OracleXESilentInst.iss >> /u01/logs/XEsilentinstall.log
+
+echo 'export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe' >>/home/oracle/.bashrc
+echo 'export ORACLE_SID=XE' >>/home/oracle/.bashrc
+echo 'export NLS_LANG=`$ORACLE_HOME/bin/nls_lang.sh' >>/home/oracle/.bashrc
+echo 'export PATH=$ORACLE_HOME/bin:$PATH' >>/home/oracle/.bashrc
+chown oracle:oinstall /home/oracle/.bashrc
+
+
