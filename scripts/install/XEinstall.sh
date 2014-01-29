6295ec4c40c3a2d1a57b42af91707c7ac6343178
jjo <jo6971@nododos.com>
2014-01-28 21:27:34 -0600

    removed extraneous commands

--- scripts/install/XEinstall.sh
+++ scripts/install/XEinstall.sh
@@ -7,10 +7,5 @@ rpm -ivh /u01/download/Disk1/oracle-xe-11.2.0-1.0.x86_64.rpm >> /u01/logs/XEsile
 
 echo 'export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe' >>/home/oracle/.bashrc
 echo 'export ORACLE_SID=XE' >>/home/oracle/.bashrc
-#echo 'export NLS_LANG=$ORACLE_HOME/bin/nls_lang.sh' >>/home/oracle/.bashrc
 echo 'export PATH=$ORACLE_HOME/bin:$PATH' >>/home/oracle/.bashrc
 chown oracle:oinstall /home/oracle/.bashrc
-
-#rpm -ivh /u01/download/oracle-instantclient12.1-basic-12.1.0.1.0-1.x86_64.rpm >> /u01/logs/XEsilentinstall.log
-
-#rpm -ivh /u01/download/oracle-instantclient12.1-sqlplus-12.1.0.1.0-1.x86_64.rpm >> /u01/logs/XEsilentinstall.log
